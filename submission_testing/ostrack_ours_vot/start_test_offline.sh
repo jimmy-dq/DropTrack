642f755f2f48f43dec52805e98a1eec163a588de
jimmy <qiangwu@stu.xmu.edu.cn>
2023-04-17 11:18:28 +0800

    Delete submission_testing directory

--- submission_testing/ostrack_ours_vot/start_test_offline.sh
+++
@@ -1,8 +0,0 @@
-cd /apdcephfs/private_qiangqwu/Projects/OSTrack_ours_v3
-# /apdcephfs/private_qiangqwu/anaconda3/envs/ostrack/bin/python tracking/analysis_results.py
-# /apdcephfs/private_qiangqwu/anaconda3/envs/ostrack/bin/python tracking/test.py ostrack vitb_384_mae_ce_32x4_ep300 --dataset nfs --threads 16 --num_gpus 4
-# /apdcephfs/private_qiangqwu/anaconda3/envs/ostrack/bin/python tracking/test.py ostrack vitb_384_mae_ce_32x4_ep299 --dataset nfs --threads 16 --num_gpus 4
-# /apdcephfs/private_qiangqwu/anaconda3/envs/ostrack/bin/python tracking/test.py ostrack vitb_384_mae_ce_32x4_ep298 --dataset nfs --threads 16 --num_gpus 4
-# /apdcephfs/private_qiangqwu/anaconda3/envs/ostrack/bin/python tracking/test.py ostrack vitb_384_mae_ce_32x4_ep297 --dataset nfs --threads 16 --num_gpus 4
-/apdcephfs/private_qiangqwu/anaconda3/envs/ostrack/bin/python tracking/test.py ostrack vitb_384_mae_ce_32x4_ep296 --dataset vot20 --threads 16 --num_gpus 4
-# /apdcephfs/private_qiangqwu/anaconda3/envs/ostrack/bin/python tracking/test.py ostrack vitb_384_mae_ce_32x4_ep295 --dataset nfs --threads 16 --num_gpus 4
